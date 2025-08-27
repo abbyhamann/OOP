75254314fa32e2a6049dfe930d7be32f21160643
abbyhamann <hamannabigail@gmail.com>
2025-08-27 09:48:50 -0500

    Add files via upload

---
+++ cw-1.py
@@ -0,0 +1,15 @@
+stu_name = input("Enter the student Name:")
+
+course1 = int(input("Enter the grade points for Course1:"))
+course2 = int(input("Enter the grade points for Course2"))
+course3 = int(input("Enter the grade points for Course3"))
+course4 = int(input("Enter the grade points for Course4"))
+course5 = int(input("Enter the grade points for Course5"))
+
+total = course1+course2+course3+course4+course5
+average = total/5
+print("The total is", total)
+print("The average is", average)
+
+name = input("Employee, what is your name?")
+wages
