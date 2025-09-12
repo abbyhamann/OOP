4414799edbb2174c6e2d349faf86ee930341426d
abbyhamann <hamannabigail@gmail.com>
2025-09-12 09:34:58 -0500

    Add files via upload

--- List Practice 1.py
+++ List Practice 1.py
@@ -1,25 +1,42 @@
-myList = []
+nameList = []
+ageList = []
 
 while True:
-    print("1. Append to the list")
-    print("2. Remove from the list")
-    print("3. Pop an element from the list")
-    print("4. Display the list")
-    print("5. Quit")
+    print("1. Add an element to the list")
+    print("2. Remove an element from the list")
+    print("3. Replace an element in the list")
+    print("4. Sort the elements in the list")
+    print("5. Reverse the elements in the list")
+    print("6. Print the list")
+    print("7. Exit")
 
     option = input("Enter your choice")
 
     if option == "1":
-        append = input("Enter what you'd like to append")
-        myList.append(append)
+        append1 = input("Enter the name of the person you'd like to add")
+        append2 = input("Enter the age of the person")
+        nameList.append(append1)
+        ageList.append(append2)
     elif option == "2":
-        remove = input("Please enter what you'd like to remove")
-        myList.remove(remove)
+        remove1 = input("Please enter the name of the person you'd like to remove")
+        remove2 = input("Please enter the age of the person you'd like to remove")
+        nameList.remove(remove1)
+        ageList.remove(remove2)
     elif option == "3":
-        myList.pop()
+        index = int(input("Please enter the index of the element you'd like to replace"))
+        newName = input("Please enter the name you'd like to replace it with")
+        newAge = input("Please enter the age you'd like to replace it with")
+        nameList[index] = newName
+        ageList[index] = newAge
     elif option == "4":
-        print(myList)
+        print("With the two lists, this will result in the ages and names getting out of order. Please choose another option")
     elif option == "5":
+        nameList.reverse()
+        ageList.reverse()
+    elif option == "6":
+        print(nameList)
+        print(ageList)
+    elif option == "7":
         break
     else:
-        print("User input is invalid. Please enter an integer 1-5")
+        print("User input is invalid. Please enter an integer 1-7")
