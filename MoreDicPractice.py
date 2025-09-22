9d7ba5e45501d5dcb25ea8326664fce39f01cc7f
abbyhamann <hamannabigail@gmail.com>
2025-09-22 09:48:51 -0500

    Add files via upload

---
+++ MoreDicPractice.py
@@ -0,0 +1,77 @@
+projects = {}
+while True:
+    print("1. Project Initiation")
+    print("2. Project CLosure")
+    print("3. Project Progress Update")
+    print("4. Print a specific Project")
+    print("5. Print all projects")
+    print("6. Quit Application")
+
+    choice = input("Please enter your choice")
+    if choice == "1":
+        key = input("Please enter the project id")
+        ptitle = input("Please enter the project title")
+        managers = input("Please enter a manager name")
+        manList = [managers]
+        moreman = input("Are there more managers? (Enter Y/N)")
+        while moreman == "Y":
+            manList.append(input("Please enter a manager name"))
+            moreman = input("Are there more managers? (Enter Y/N)")
+        start_date = input("Please enter the start date of the project")
+        end_date = input("Please enter the end date of the project")
+        sponsor = input("Please enter the sponsor of the project")
+        budget = input("Please enter the budget for the project")
+        tech = input("Please enter the technologies for the project")
+        techList = [tech]
+        moretech = input("Are there more technologies? (Enter Y/N)")
+        while moretech == "Y":
+            techList.append(input("Please enter a technology for the project"))
+            moretech = input("Are there more technologies? (Enter Y/N)")
+        team_members = input("Please enter a member of the project")
+        memberList = [team_members]
+        moremem = input("Are there more members? (Enter Y/N)")
+        while moremem == "Y":
+            techList.append(input("Please enter a member"))
+            memberList = input("Are there more members? (Enter Y/N)")
+
+
+        projects.update({key:{
+            "title": ptitle,
+            "managers": manList,
+            "start_date":start_date,
+            "end_date":end_date,
+            "sponsor":sponsor,
+            "budget": budget,
+            "tech":techList,
+            "member": memberList,
+                        }})
+    elif choice == "2":
+        key = input("Please enter the project id you'd like to close")
+        del projects[key]
+    elif choice == "3":
+        key = input("Please enter the project id you'd like to update")
+        element = input("Please enter the element you'd like to update. Options: title, managers, start_date, end_date, sponsor, budget, tech, member")
+        name = input("Please enter the name of the student")
+        lab1 = int(input("Please enter the points the student earned for Lab 1"))
+        lab2 = int(input("Please enter the points the student earned for Lab 2"))
+        lab3 = int(input("Please enter the points the student earned for Lab 3"))
+        lab4 = int(input("Please enter the points the student earned for Lab 4"))
+        lab5 = int(input("Please enter the points the student earned for Lab 5"))
+        total = lab1 + lab2 + lab3 + lab4 + lab5
+        percent = total / 50 * 100
+        average = total / 5
+        myStudents.update({key:{
+            "name":name,
+            "lab1":lab1,
+            "lab2": lab2,
+            "lab3": lab3,
+            "lab4": lab4,
+            "lab5": lab5,
+            "total": total,
+            "percent": percent,
+            "average":average
+        }})
+    elif choice == "4":
+        print(myStudents)
+    elif choice == "5":
+        break
