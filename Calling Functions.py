4c4c99deac5ace77362e5d71da6ca925b6225b00
abbyhamann <hamannabigail@gmail.com>
2025-09-26 09:31:12 -0500

    Add files via upload

---
+++ Calling Functions.py
@@ -0,0 +1,17 @@
+myList = []
+a=10
+
+def function1(r):
+    r=r+1
+    area = 3.14*r**2
+    return area
+
+def function2():
+    a = 50
+    b="Justus"
+
+while True:
+    radius = 10
+    area = function1(radius)
+    print(area)
+    break
