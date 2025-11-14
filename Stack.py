179c6df2ea862e4624d07b55db0fff38e51e3480
abbyhamann <hamannabigail@gmail.com>
2025-11-14 09:52:42 -0600

    Add files via upload

--- Stack.py
+++ Stack.py
@@ -4,23 +4,28 @@ top = tk.Tk()
 top.geometry("500x500")
 text_Box = tk.Text(top, width=35, height=2)
 text_Box.place(x=50, y=50)
+text_Box2 = tk.Text(top, width=35, height=2)
+text_Box2.place(x=50, y=300)
+text_Box3 = tk.Text(top, width=35, height=2)
+text_Box3.place(x=50, y=400)
 
 class Queue:
     def __init__(self):
         self.element = []
+
     def enqueue(self):
-        textBox = tk.Text(top, width=35, height=2)
-        x = textBox.get(1.0, "end-1c")
-        self.element.append(x)
+        self.element.append(text_Box.get(1.0, "end-1c"))
+
     def dequeue(self):
         self.element.remove(self.element[0])
-    def display(self):
-        print("Elements in Queue:")
+    def display(self)
+
         for i in self.element:
-            print(i)
+            text_Box3.insert(tk.INSERT, i)
 class Stack:
     def __init__(self):
         self.element = []
+
 
     def push(self):
         self.element.append((text_Box.get(1.0, "end-1c")))
@@ -31,7 +36,7 @@ class Stack:
     def display(self):
         print("Elements in Stack:")
         for i in self.element:
-            print(i)
+            text_Box2.insert(tk.INSERT, i)
 
 
 s = None
@@ -40,6 +45,7 @@ i = 1
 x=1
 stacks = {}
 queues = {}
+
 
 
 def show(n):
@@ -51,12 +57,11 @@ def show(n):
         name = "s" + str(i)
         stacks[name] = Stack()
         s = stacks[name]
-        print("Created new stack:", name)
         i += 1
     elif n == "Push":
         if s:
             s.push()
-            text_Box
+            text_Box.delete("1.0","end")
             print("Element pushed")
         else:
             print("First, make a stack")
@@ -66,7 +71,7 @@ def show(n):
             print("Element popped")
         else:
             print("First, make a stack")
-    elif n == "Display":
+    elif n == "Display Stack":
         if s:
             s.display()
         else:
@@ -76,11 +81,11 @@ def show(n):
         name = "q" + str(x)
         queues[name] = Queue()
         q = queues[name]
-        print("Created new queue:", name)
         x += 1
     elif n == "Enqueue":
         if q:
             q.enqueue()
+            text_Box.delete("1.0", "end")
             print("Element added to end of queue")
         else:
             print("First, make a queue")
@@ -90,7 +95,7 @@ def show(n):
             print("Element removed from front of queue")
         else:
             print("First, make a queue")
-    elif n == "Display":
+    elif n == "Display Queue":
         if q:
             q.display()
         else:
@@ -107,7 +112,7 @@ B2.place(x=150, y=100)
 B3 = tk.Button(top, text="Pop", width=10, height=4, command=lambda: show("Pop"))
 B3.place(x=250, y=100)
 
-B4 = tk.Button(top, text="Display", width=10, height=4, command=lambda: show("Display"))
+B4 = tk.Button(top, text="Display", width=10, height=4, command=lambda: show("Display Stack"))
 B4.place(x=350, y=100)
 
 B5 = tk.Button(top, text="Create Queue", width=10, height=4, command=lambda: show("Create Queue"))
@@ -119,7 +124,7 @@ B6.place(x=150, y=200)
 B7 = tk.Button(top, text="Dequeue", width=10, height=4, command=lambda: show("Dequeue"))
 B7.place(x=250, y=200)
 
-B8 = tk.Button(top, text="Display", width=10, height=4, command=lambda: show("Display"))
+B8 = tk.Button(top, text="Display", width=10, height=4, command=lambda: show("Display Queue"))
 B8.place(x=350, y=200)
 
 top.mainloop()
